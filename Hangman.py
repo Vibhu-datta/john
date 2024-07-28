204b1dc38b11b39d2bffe76a8c3068bbe5ad5236
Vibhu-datta <Vibhudatta3@gmail.com>
2024-07-28 13:14:23 -0400

    function

--- Hangman.py
+++ Hangman.py
@@ -1,19 +1,6 @@
-word='tree'
 
-# show image
-# show the word 
-wrong_guesses=0
-guessed_letters = []
 
-while wrong_guesses < 6:
-    # ask user to guess letter
-    user_input= input("Guess Letter:")
-    # check if letter is apart of word
-    if user_input in word:
-        guessed_letters.append(user_input)
-    else:
-        wrong_guesses=wrong_guesses+1
-    
+def show_letter(word, guessed_letters):
     for letter in word:
         if letter in guessed_letters:
             print(letter, end="")
@@ -21,7 +8,27 @@ while wrong_guesses < 6:
             print("_", end="")
     print("\n")
 
-print("you lose")
+
+
+# show image
+# show the word 
+
+def hangman():
+    word='tree'
+    wrong_guesses=0
+    guessed_letters = []
+    while wrong_guesses < 6:
+    # ask user to guess letter
+        user_input= input("Guess Letter:")
+        # check if letter is apart of word
+        if user_input in word:
+            guessed_letters.append(user_input)
+        else:
+            wrong_guesses=wrong_guesses+1
+        show_letter(word, guessed_letters)
+    print("you lose")
+
+hangman()
     # if in word add letter 
     # if not add body partb
     # repeat process 
