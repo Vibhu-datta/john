6b36d4e0ef19a0ded2d42813166b610b3fb73593
Vibhu-datta <Vibhudatta3@gmail.com>
2024-08-21 15:39:12 -0400

    english code

---
+++ Blackjack.py
@@ -0,0 +1,11 @@
+#create deck
+#shuffle deck
+#deal two cards player and computer
+#show player their cards and computer's first card
+#ask player if they want to hit or stand
+#repeat until player stands or busts
+#show dealers remaining card
+#if computers card below 17 hit
+#repeat until computer cards are above 17 or busts
+#compare cards and declare winner
+#ask player if they want to play again
